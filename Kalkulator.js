64b016ea38e0cdcafeb30fbd5c52721289a05348
Redi Ahmad Supriyatna <ahmadredi67@gmail.com>
2022-05-16 11:37:35 +0700

    Menambahkan objek calculator

--- Kalkulator.js
+++ Kalkulator.js
@@ -1,1 +1,9 @@
 console.log("Web Calculator by Dicoding");
+
+// objek untuk menyimpan data dan kondisi pada kalkulator
+const calculator = {
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
+    waitingForSecondNumber: false
+};
