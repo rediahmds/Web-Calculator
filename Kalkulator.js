f6e2e67d69ff55d100a60535cf2a616d08aa0097
Redi Ahmad Supriyatna <ahmadredi67@gmail.com>
2022-05-16 23:15:18 +0700

    Menambahkan objek calculator dan function updateCalc, clearCalc, dan inputDigit serta event listener pada button

--- Kalkulator.js
+++ Kalkulator.js
@@ -7,3 +7,35 @@ const calculator = {
     firstNumber: null,
     waitingForSecondNumber: false
 };
+
+function updateDisplay() {
+    document.querySelector("#displayNumber").innerText = calculator.displayNumber;
+}
+
+function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.waitingForSecondNumber = false;
+}
+
+function inputDigit(putNumber) {
+    if (calculator.displayNumber === '0') {
+        calculator.displayNumber = putNumber;
+    }
+    else {
+        calculator.displayNumber += putNumber;
+    }
+}
+
+const buttons = document.querySelectorAll(".button");
+for (let button of buttons) {
+    button.addEventListener('click', function (event) {
+
+        // mendapatkan objek elemen yang diklik
+        const target = event.target;
+
+        inputDigit(target.innerText);
+        updateDisplay()
+    });
+}
